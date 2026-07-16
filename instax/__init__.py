8b4ddd79683ce122a2270f35456eace521f6bc8a
liushuai <770722922@qq.com>
2026-07-16 22:08:12 +0800

    refactor: extract Python mode registry

---
+++ instax/__init__.py
@@ -0,0 +1,5 @@
+"""Reusable building blocks for the Instax filter."""
+
+from .config import MODE_CONFIGS, MODE_DESCRIPTIONS, ModeConfig, get_mode_config
+
+__all__ = ["MODE_CONFIGS", "MODE_DESCRIPTIONS", "ModeConfig", "get_mode_config"]
