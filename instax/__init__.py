474d4e9122ff9578fcf9d8e2b27eb28fb9621c90
liushuai <770722922@qq.com>
2026-07-16 22:09:58 +0800

    refactor: extract Python processing engine

--- instax/__init__.py
+++ instax/__init__.py
@@ -1,5 +1,6 @@
 """Reusable building blocks for the Instax filter."""
 
 from .config import MODE_CONFIGS, MODE_DESCRIPTIONS, ModeConfig, get_mode_config
+from .engine import apply_instax_look
 
-__all__ = ["MODE_CONFIGS", "MODE_DESCRIPTIONS", "ModeConfig", "get_mode_config"]
+__all__ = ["MODE_CONFIGS", "MODE_DESCRIPTIONS", "ModeConfig", "apply_instax_look", "get_mode_config"]
